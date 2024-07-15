3a260044650196d54315b130c60c31da66d94089
eholm62 <elliot.b.holmes@gmail.com>
2024-07-15 16:24:29 -0400

    configured constants for windows powershell use

--- Constants.java
+++ Constants.java
@@ -1,8 +1,8 @@
 public class Constants {
-	public static final int SCREEN_WIDTH = 170;
+	public static final int SCREEN_WIDTH = 183;
 	public static final int SCREEN_HEIGHT = 45;
 
-	public static final double Y_STRETCH = 2.15;
+	public static final double Y_STRETCH = 2.0;
 	
 	public static final int FPS = 30;
 }
