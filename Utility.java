64db3cc93792535aab0fed8598b8bdd1853c7dc6
Elliot Holmes <133877996+eholm62@users.noreply.github.com>
2024-07-15 20:04:04 +0000

    changed rotating square such that each chixel is a square rather than an @ symbol

---
+++ Utility.java
@@ -0,0 +1,12 @@
+public class Utility {
+    public static double distance(double x1, double y1, double x2, double y2) {
+		double xDiff = x2 - x1;
+		double yDiff = y2 - y1;
+		return Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+	}
+
+    public static void clearScreen() {  
+		System.out.print("\033[2J");  
+		System.out.flush();  
+	}  
+}
